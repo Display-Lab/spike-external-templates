ed4c7bd13d2c176091bf73ba08b853ae1d5d57b8
mradamgbeck <mradamgbeck@gmail.com>
2022-01-03 13:43:47 -0500

    initial commit

---
+++ run_candidate_smasher.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# This is designed to be run from within a directory that contains the following files:
+# annotations.r
+# spek.json
+# performance.csv
+# templates.json
+# causal_pathways.json
+
+COL_WIDTH=30
+LOG_FILE=vignette.log
+
+# Create outputs directory
+mkdir -p outputs
+
+# Create log file
+date > ${LOG_FILE}
+# Run candidate smasher on spek (spek_bs.json) and templates
+printf "%-${COL_WIDTH}s" "Running CandidateSmasher..." | tee -a ${LOG_FILE}
+printf "\n" >> ${LOG_FILE}
+$DISPLAY_LAB_HOME/candidate-smasher/bin/cansmash --path=outputs/spek_bs.json --md-source=templates.json 2>> ${LOG_FILE} | jq . > outputs/spek_cs.json
+printf "exit status: %d\n" "${?}"
+
+printf "Log written to ${LOG_FILE}\n"
